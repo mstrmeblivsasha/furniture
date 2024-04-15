fc4bbf6fa76ec9e3b8cbc778adab711c7fdc3d45
PolztiukVolodymyr <92870556+PolztiukVolodymyr@users.noreply.github.com>
2024-04-15 20:21:30 +0300

    resolve conflict

--- src/sections/contactUsSection/ContactUsSection.tsx
+++ src/sections/contactUsSection/ContactUsSection.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import ContactForm from "@/components/ContactForm/ContactForm";
+import styles from "./ContactUsSection.module.scss";
 
 const ContactUsSection = () => {
-    return <div>ContactUsSection</div>;
+    return (
+        <section>
+            <div className={`container ${styles.container} `}>
+                <h2 className={styles.title}>
+                    <span>Маєте питання?</span>
+                </h2>
+                <ContactForm />
+            </div>
+        </section>
+    );
 };
 
 export default ContactUsSection;

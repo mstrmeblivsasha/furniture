492aa0475f6d622be3e5b0e0e83fa56001577a1b
PolztiukVolodymyr <92870556+PolztiukVolodymyr@users.noreply.github.com>
2024-04-16 21:17:01 +0300

    add contact form

--- src/zodShemas/contactsShema.ts
+++ src/zodShemas/contactsShema.ts
@@ -5,10 +5,20 @@ const regexPhone = /^\+\d{12}$/;
 export const contactsSchema = z.object({
     userName: z
         .string()
-        .min(3, { message: "Username must be at least 3 characters" }),
-    phone: z.string().regex(regexPhone, "+380123456789"),
-    message: z.string().optional(),
+        .trim()
+        .min(1, { message: "Заповніть це поле" })
+        .min(3, { message: "Занадто коротке" })
+        .max(40, { message: "Забагато символів" }),
+    phone: z
+        .string()
+        .regex(regexPhone, "+380123456789")
+        .max(16, { message: "Забагато символів" }),
+    message: z
+        .string()
+        .trim()
+        .min(1, { message: "Заповніть це поле" })
+        .max(240, { message: "Too long" }),
 });
 
 //extract the inferred type from schema
-export type ContactsSchemaType = z.infer<typeof contactsSchema>;
+export type TypeContactsSchema = z.infer<typeof contactsSchema>;

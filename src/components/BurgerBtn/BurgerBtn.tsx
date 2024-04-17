f1f44351aefddca2445a6201662571c54e498b1c
Nataliia <natalabojko@gmail.com>
2024-04-17 12:20:18 +0300

    siteContext organised

--- src/components/BurgerBtn/BurgerBtn.tsx
+++ src/components/BurgerBtn/BurgerBtn.tsx
@@ -1,14 +1,29 @@
+"use client";
+
 import React, { useContext } from "react";
 import styles from "./BurgerBtn.module.scss";
+import { SiteContext } from "@/context/SiteContext";
 
 const BurgerBtn = () => {
-  // const {   burgerMenu,
-  //   setBurgermenu}  =   useContext(SiteContext);
+  const { isMobileMenu, setIsMobileMenu } = useContext(SiteContext);
+  // console.log("isMobileMenu", isMobileMenu);
+
   return (
-    <button className={styles.burgerBtn}>
-      <svg>
-        <use href="./sprite.svg#icon-burger"></use>
-      </svg>
+    <button
+      className={styles.burgerBtn}
+      onClick={() => {
+        setIsMobileMenu(!isMobileMenu);
+      }}
+    >
+      {isMobileMenu ? (
+        <svg>
+          <use href="./sprite.svg#icon-burgerclose"></use>
+        </svg>
+      ) : (
+        <svg>
+          <use href="./sprite.svg#icon-burger"></use>
+        </svg>
+      )}
     </button>
   );
 };
--- src/sections/heroSection/HeroSection.tsx
+++ src/sections/heroSection/HeroSection.tsx
@@ -1,10 +1,22 @@
+"use client";
+
+// import { useSiteContext } from "@/context/SiteContext";
 import styles from "./HeroSection.module.scss";
 
 const HeroSection = () => {
+  // const { name, setName } = useSiteContext();
   return (
     <section>
       <div className={`container ${styles.container} `}>
         <h1>Hero start</h1>
+        {/* {name}
+        <button
+          onClick={() => {
+            name === "Vika" ? setName("Natala") : setName("Vika");
+          }}
+        >
+          setName
+        </button> */}
       </div>
     </section>
   );

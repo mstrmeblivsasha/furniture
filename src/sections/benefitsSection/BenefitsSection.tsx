9d29716c4be35c78837cfa43aa07a4e08026a88e
Nataliia <natalabojko@gmail.com>
2024-05-01 18:13:23 +0300

    why us

--- src/sections/benefitsSection/BenefitsSection.tsx
+++ src/sections/benefitsSection/BenefitsSection.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import styles from "./BenefitsSection.module.scss";
 import { benefits } from "../../data/benefits";
+import { useEffect } from "react";
 
 type elProps = {
   id: number;
@@ -8,43 +11,38 @@ type elProps = {
 };
 
 const BenefitsSection = () => {
-  console.log(benefits);
+  //   useEffect(() => {
+  //     const itemClassname = () => {
+  //       if (typeof document !== "undefined") {
+  //         const listItems: NodeListOf<HTMLLIElement> =
+  //           document.querySelectorAll(".benefitsList li");
+  //         listItems.forEach((item: HTMLLIElement) => {
+  //           console.log("item", item);
+  //           const width: number = item.offsetWidth;
+  //           item.style.height = `${width}px`;
+  //         });
+  //       }
+  //     };
+  //     itemClassname(); // Викликаємо функцію після монтажу компонента
+  //   }, []);
 
   return (
     <section className={styles.section}>
       <div className={`container ${styles.container}`}>
-        <h2 className="sectionTitle">
+        <h2 className={`sectionTitle ${styles.sectionTitle}`}>
           <span>Чому варто обрати мої послуги?</span>
         </h2>
-        <ul>
+        <ul className={styles.benefitsList}>
           {benefits.map((el: elProps) => {
             return (
               <li key={el.id}>
+                <div className={styles.itemNumber}>{el.id}</div>
                 <h3>{el.title}</h3>
+                <p>{el.text}</p>
               </li>
             );
           })}
         </ul>
-        {/* <div className={styles.aboutWrapp}>
-          <div className={styles.imgWrapp}>
-            <Image src="/images/Oleksandr.webp" fill alt="Олександр" />
-          </div> 
-          <div className={styles.aboutDescr}>
-            <p>
-              Мене звати Олександр і я - Ваш персональний майстер з виготовлення
-              меблів з п&#39;ятирічним досвідом у цій сфері.
-            </p>
-            <p>
-              Я завжди прагну втілити Ваші ідеї у реальність. Незалежно від
-              того, чи Ви шукаєте класичні, сучасні або ексклюзивні меблі, я
-              готовий працювати з Вами крок за кроком.
-            </p>
-            <p>
-              Моя мета - зробити ваш будинок більш комфортним та стильним,
-              створивши меблі, які відображатимуть вашу унікальну особистість.
-            </p>
-          </div>
-        </div> */}
       </div>
     </section>
   );
